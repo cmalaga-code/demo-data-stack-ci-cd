18b1b231702e8636c4a441945732ddd08ea71a33
Cristian Malaga <cmalaga720@gmail.com>
2025-07-04 02:00:24 -0400

    Full Deploy Last

--- sql/01_create_structure.sql
+++ sql/01_create_structure.sql
@@ -1,41 +1,15 @@
-BEGIN
-  IF NOT EXISTS (
-    SELECT * FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_NAME = 'FACT_CLAIMS'
-      AND TABLE_SCHEMA = CURRENT_SCHEMA()
-  ) THEN
-    EXECUTE IMMEDIATE $$
-      CREATE TABLE FACT_CLAIMS (
-        id INT,
-        member_id INT,
-        provider_id INT
-      );
-    $$;
-  END IF;
+CREATE TABLE IF NOT EXISTS FACT_CLAIMS (
+  id INT,
+  member_id INT,
+  provider_id INT
+);
 
-  IF NOT EXISTS (
-    SELECT * FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_NAME = 'DIM_MEMBER'
-      AND TABLE_SCHEMA = CURRENT_SCHEMA()
-  ) THEN
-    EXECUTE IMMEDIATE $$
-      CREATE TABLE DIM_MEMBER (
-        member_id INT,
-        name STRING
-      );
-    $$;
-  END IF;
-  
-  IF NOT EXISTS (
-    SELECT * FROM INFORMATION_SCHEMA.TABLES
-    WHERE TABLE_NAME = 'DIM_PROVIDER'
-      AND TABLE_SCHEMA = CURRENT_SCHEMA()
-  ) THEN
-    EXECUTE IMMEDIATE $$
-      CREATE TABLE DIM_PROVIDER (
-        provider_id INT,
-        name STRING
-      );
-    $$;
-  END IF;
-END;
+CREATE TABLE IF NOT EXISTS DIM_MEMBER (
+  member_id INT,
+  name STRING
+);
+
+CREATE TABLE IF NOT EXISTS DIM_PROVIDER (
+  provider_id INT,
+  name STRING
+);
