6f609ec629356cbb09bd2006fdcef06ee238cc57
Cristian Malaga <cmalaga720@gmail.com>
2025-07-10 01:36:37 -0700

    Deploy Stack

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import boto3
-from aws_cdk import App
+from aws_cdk import App, aws_s3 as s3, RemovalPolicy
 from botocore.exceptions import ClientError
 
 from orchestration_stack.step_function_construct import OrchestrationStack
@@ -21,21 +21,18 @@ from compute_stack.glue_stack.glue_construct import (
     UnStructuredApplicationDataGlueStack
 )
 
-def check_env_vars(required_vars):
-    missing = [var for var in required_vars if not os.environ.get(var)]
-    if missing:
-        raise ValueError(f"Missing required environment variables: {', '.join(missing)}")
-
 def bucket_exists(bucket_name: str) -> bool:
     s3_client = boto3.client("s3")
     try:
         s3_client.head_bucket(Bucket=bucket_name)
         return True
     except ClientError as e:
-        error_code = e.response["Error"]["Code"]
-        if error_code == "404":
-            return False
-        raise
+        return e.response["Error"]["Code"] != "404"
+
+def check_env_vars(required_vars):
+    missing = [var for var in required_vars if not os.environ.get(var)]
+    if missing:
+        raise ValueError(f"Missing required environment variables: {', '.join(missing)}")
 
 # App entry point
 app = App()
@@ -43,6 +40,7 @@ app = App()
 #  Ensure required env vars are set
 check_env_vars(["STAGE_BUCKET", "CURATED_BUCKET", "APPLICATION_BUCKET", "ENV"])
 deployment_env = os.environ["ENV"]
+is_dev = str(deployment_env).upper() == "DEV"
 
 # Lambda stacks
 structured_curated_lambda_stack = StructuredCurateDataLambdaStack(app, "structured-curated-lambda-stack")
@@ -75,8 +73,44 @@ orchestration_stack = OrchestrationStack(
     snowflake_model_claims_lambda_stack.fn
 )
 
-# Finalized Lambda + buckets + event notification
-meta_lambda_stack = MetaLambdaStack(app, "meta-lambda-stack", orchestration_stack, env_name=deployment_env) 
+#  Create all three buckets
+if bucket_exists(os.environ["STAGE_BUCKET"]):
+    stage_bucket = s3.Bucket.from_bucket_name(app, "ImportedStageBucket", os.environ["STAGE_BUCKET"])
+else:
+    stage_bucket = s3.Bucket(
+        app, "StageBucket",
+        bucket_name=os.environ["STAGE_BUCKET"],
+        versioned=True,
+        removal_policy=RemovalPolicy.DESTROY if is_dev else RemovalPolicy.RETAIN,
+        auto_delete_objects=is_dev
+    )
+if bucket_exists(os.environ["CURATED_BUCKET"]):
+    curated_bucket = s3.Bucket.from_bucket_name(app, "ImportedCuratedBucket", os.environ["CURATED_BUCKET"])
+else:
+    curated_bucket = s3.Bucket(
+        app, "CuratedBucket",
+        bucket_name=os.environ["CURATED_BUCKET"],
+        versioned=True,
+        removal_policy=RemovalPolicy.DESTROY if is_dev else RemovalPolicy.RETAIN,
+        auto_delete_objects=is_dev
+    )
+
+if bucket_exists(os.environ["APPLICATION_BUCKET"]):
+   application_bucket = s3.Bucket.from_bucket_name(app, "ImportedApplicationBucket", os.environ["APPLICATION_BUCKET"])
+else:
+    application_bucket = s3.Bucket(
+        app, "ApplicationBucket",
+        bucket_name=os.environ["APPLICATION_BUCKET"],
+        versioned=True,
+        removal_policy=RemovalPolicy.DESTROY if is_dev else RemovalPolicy.RETAIN,
+        auto_delete_objects=is_dev
+    )
+
+
+meta_lambda_stack = MetaLambdaStack(
+    app, "meta-lambda-stack", orchestration_stack, env_name=deployment_env, 
+    stage_bucket=stage_bucket, curated_bucket=curated_bucket, application_bucket=application_bucket
+) 
 
 # Synthesize to generate CloudFormation templates
 app.synth()
